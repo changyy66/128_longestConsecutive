2c6fbd16126a29adc37ef960093426da17a2a9ac
change <changyy66@163.com>
2019-10-13 23:03:17 +0800

    add

--- 128_longestConsecutive/128_longestConsecutive.cpp
+++ 128_longestConsecutive/128_longestConsecutive.cpp
@@ -2,7 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <set>
+using namespace std;
 
+class Solution {
+public:
+	int longestConsecutive(vector<int>& nums) {
+		set<int> s;
+		for (int i = 0; i < nums.size(); i++)
+			s.insert(nums[i]);
+		int maxNum = 0;
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (s.count(nums[i] - 1) == 0) {
+				int n = 1;
+				while (s.count(nums[i] + n))
+				{
+					n++;
+				}
+				if (maxNum < n)maxNum = n;
+			}
+		}
+		return maxNum;
+	}
+};
 
 int main()
 {
